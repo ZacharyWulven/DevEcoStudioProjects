019b4eedb1c9286c558e8004556d6154be11bba7
Hogetsu <hogetsu@HogetsudeMacBook-Pro.local>
2024-10-17 00:39:32 +0800

    ts 多类型

--- HelloTS/hello.ts
+++ HelloTS/hello.ts
@@ -1,3 +1,10 @@
 // 创建tsconfig.json。 输入命令tsc --init
 
 console.log('hello world !');
+
+let age = 10;
+console.log('age is:', age);
+
+let sex: 'man' | 'woman' | 'secret' = 'man';
+console.log('sex is:', sex);
+// sex = 'a'   // Error 
